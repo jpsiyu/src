f6447606164bb1b753eae5d1dc4649cbddb6a34a
jpsiyu <jpsiyu@163.com>
2014-05-02 20:08:05 +0800

    src updated

--- erlang/dntg/include/predefine.hrl
+++
@@ -1,9 +0,0 @@
-%% 场景
--define(HB_NPC_ID, 10206).      %% 皇榜npcid
--define(MAIN_CITY_SCENE, 102).  %% 主城id
--define(MAIN_CITY_RAND_XY, [{35,61},{31,57},{31,51},{35,47},{41,47},{45,51},{45,57},{41,60}]). %% 主城随机出生坐标点
--define(PRISON_SCENE, 998).     %% 监狱场景id
-
-
-%% 技能buff
--define(COIN_DUN_COMBO_SKILL_ID, 900002). %% 铜钱副本连斩buff
--- erlang/dntg/script/gs10.bat
+++
@@ -1,5 +0,0 @@
-cd ../
-#del ebin\*.beam
-erl -noshell -s make all -s init stop
-cd config
-erl +P 1024000 +K true -smp disable -name yxyz10@127.0.0.1 -setcookie gs -boot start_sasl -config gs -pa ../ebin -s gs start -extra 192.168.1.61 9010 10
